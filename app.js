9028aac454084bfb906840a7fe0d59475a1158b3
JaeCholJeon <wjswocjf92@gmail.com>
2019-01-15 23:07:53 +0900

    fix : import bootstrap
    fix : import bootstrap

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ var authUser =function(id,pwd,callback){
 app.set('port',process.env.PORT||9000);
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+app.use('/css',express.static(__dirname+'/node_modules/bootstrap/dist/css'));
+app.use('/js',express.static(__dirname+'/node_modules/bootstrap/dist/js'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(expressSession({
